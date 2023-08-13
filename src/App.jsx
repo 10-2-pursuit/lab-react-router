88861dbd3141a4879b111f6ab537bdd42c76856d
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-08-13 17:26:55 -0400

    Add basic routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 /*
   Components
@@ -27,9 +28,16 @@ function App() {
   return (
     <div className="wrapper">
       <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home employees={employees} owners={owners} pets={pets} />}
+          />
+          <Route path="/staff" element={<StaffList employees={employees} />} />
+          <Route path="/pets" element={<PetsList pets={pets} />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
